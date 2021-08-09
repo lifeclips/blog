861851f44389b6c7dcd139d83e454184d6c48ccd
hsywme <hsyw@me.com>
2021-08-09 20:31:18 +0800

    update dark mode highlighting

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var theme = document.getElementById("highlight-theme");
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      theme.href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/github-dark-dimmed.min.css";
+}
+
+if(window.matchMedia('(prefers-color-scheme: light)').matches){
+      theme.href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/github.min.css";
+}
+
+let listeners={
+  dark:(mediaQueryList )=>{
+    if(mediaQueryList.matches){
+      theme.href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/github-dark-dimmed.min.css";
+    }
+  },
+  light:(mediaQueryList)=>{
+    if(mediaQueryList.matches){
+      theme.href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/github.min.css";
+    }
+  }
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addListener(listeners.dark)
+window.matchMedia('(prefers-color-scheme: light)').addListener(listeners.light)
